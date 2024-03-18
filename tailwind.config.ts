e9d3ce5695d0f6dcf8fb0ff51169f5b14f45adaa
Badejo Emmanuel <badejoemmanuel805@gmail.com>
2024-03-18 08:19:51 +0100

    (Add assets and components Files )Create a Starting BoilerPlate for Main Page.

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,11 +8,16 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
+      colors: {
+        lightOpacityBlue: "#EAF3FF",
+        hoverBlue: "#0047A4",
+        SpecialBlue: "#1A73E8",
+        primaryOrange: "#DC6803",
+        primaryBlue: "#1A73E8",
+        primaryGreen: "#039855",
+        grayishAsh: "#98A2B3",
+        lightGray: "#D0D5DD",
+      }
     },
   },
   plugins: [],
