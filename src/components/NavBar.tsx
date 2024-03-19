62a422d98bfb98d3cca23abeecdb76065e846c58
Badejo Emmanuel <badejoemmanuel805@gmail.com>
2024-03-19 02:56:13 +0100

    (create NavBar ui component)

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,9 +1,54 @@
-import React from 'react'
+import Button from "@/utils/Button";
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+import { RxCaretDown } from "react-icons/rx";
 
 const NavBar = () => {
-  return (
-    <div>NavBar</div>
-  )
-}
+    return (
+        <header className="bg-white z-[10000] left-0 right-0 shadow-inner   fixed w-full top-0  h-[5.25rem] flex items-center">
+            <div className="container bg-white items-center mx-auto xl:w-[75%] flex justify-between">
+                <div className="flex bg-white gap-8 items-center">
+                    <Link href={"/"}>
+                        <Image
+                            src={"/Logo.svg"}
+                            alt="Logo"
+                            width={120}
+                            height={100}
+                        />
+                    </Link>
+                    <nav>
+                        <ul className="flex space-x-8">
+                            <li>Home</li>
+                            <li className="flex items-center">
+                                <span>Products</span>{" "}
+                                <span>
+                                    <RxCaretDown className="text-2xl" />
+                                </span>
+                            </li>
+                            <li>Candidate</li>
+                            <li>Pricing</li>
+                            <li className="flex items-center">
+                                <span>Resources</span>{" "}
+                                <span>
+                                    <RxCaretDown className="text-2xl" />
+                                </span>
+                            </li>
+                        </ul>
+                    </nav>
+                </div>
 
-export default NavBar
+                <div className="space-x-4">
+                    <Button className="bg-lightOpacityBlue  text-blue-800 font-semibold px-5 rounded-lg p-3">
+                        Login
+                    </Button>
+                    <Button className="bg-primaryBlue  text-white font-semibold px-5 rounded-lg p-3">
+                        Book a Demo
+                    </Button>
+                </div>
+            </div>
+        </header>
+    );
+};
+
+export default NavBar;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,6 +5,7 @@ const config: Config = {
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  
   ],
   theme: {
     extend: {
@@ -17,6 +18,9 @@ const config: Config = {
         primaryGreen: "#039855",
         grayishAsh: "#98A2B3",
         lightGray: "#D0D5DD",
+      }  ,
+      backgroundImage: {
+        heroBg: "url('/LoubbyHeroBgSection.webp')"
       }
     },
   },
