59097936881574056fc18d8cf217b0cca773e784
Badejo Emmanuel <badejoemmanuel805@gmail.com>
2024-03-27 20:57:16 +0100

    Fixed overflow of images in the slideshow on Mobile view

--- src/components/UI/SlideShowHeader.tsx/SlideShowImages.tsx
+++ src/components/UI/SlideShowHeader.tsx/SlideShowImages.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 
 export const SlideShowImage = ({ context }: { context: any }) => {
     return (
-        <div className="md:flex space-y-5 relative px-4 items-center justify-between w-full xl:w-[75%] mx-auto">
+        <div className="md:flex md:space-y-5 space-y-9 relative px-4 items-center justify-between w-full xl:w-[75%] mx-auto">
             <div className="md:w-[45%] space-y-4">
                 <h1
                     style={{ color: context?.color }}
@@ -17,7 +17,7 @@ export const SlideShowImage = ({ context }: { context: any }) => {
                 <p className="md:text-lg">{context?.body}</p>
             </div>
             <div className="relative h-auto mr-8">
-                <div className="lg:w-[30rem] w-[26rem]">
+                <div className="lg:w-[30rem] w-full md:w-[26rem]">
                     <div
                         style={{ borderColor: context?.color }}
                         className="lg:h-[28rem] h-[24rem] md:h-[20rem]  md:w-full  border-8  rounded-xl"
@@ -26,7 +26,7 @@ export const SlideShowImage = ({ context }: { context: any }) => {
                             src={context?.img}
                             alt="slideShowImage"
                             width={700}
-                            height={700}
+                            height={700 }
                             className="rounded-2xl  xl:h-[28rem] h-[24rem] md:h-[20rem] top-16 left-10 drop-shadow-md absolute"
                         />
                     </div>
