013a52fec43605e42f898d5e671d01e98ff23df3
Badejo Emmanuel <badejoemmanuel805@gmail.com>
2024-03-28 11:02:17 +0100

    Modified Footer component (Add Mobile Responsiveness to the Footer links)

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -12,8 +12,8 @@ import { FaTwitter } from "react-icons/fa";
 const Footer = () => {
     return (
         <div className="container mx-auto px-4 md:px-0 xl:w-[75%]">
-            <div className="flex  flex-col md:flex-row items-center gap-40 py-12 mb-12 ">
-                <div className="space-y-8">
+            <div className="flex  flex-col md:flex-row  gap-12 md:gap-40 py-12 mb-12 ">
+                <div className="md:space-y-8">
                     <Link href={"/"}>
                         <Image
                             src={"/Logo.svg"}
@@ -24,7 +24,7 @@ const Footer = () => {
                     </Link>
                     <p>All-in-one hiring solution for remote African talents</p>
                 </div>
-                <div className="flex flex-col items-center text-center md:text-start space-y-8 md:space-y-0 md:items-start lg:flex-row justify-between flex-1">
+                <div className="flex flex-wrap  text-center md:text-start space-y-8 md:space-y-0 md:items-start lg:flex-row justify-between flex-1">
                     <div>
                         <div className="font-semibold py-1">Product</div>
                         <ul className="space-y-3">
@@ -77,7 +77,7 @@ const Footer = () => {
             </div>
             <div>
                 <div className="border-t h-[.2rem]" />
-                <div className="flex py-8 justify-between">
+                <div className="md:flex px-2 md:px-0 space-y-5 md:space-y-0 py-8 justify-between">
                     <div>&copy; 2023 Loubby AI</div>
                     <div className="flex  space-x-3 md:space-x-8">
                         <Link href={"/"}>
