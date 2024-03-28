c745dd8dbb509a9655d43764bc5d523a3e23948a
Badejo Emmanuel <badejoemmanuel805@gmail.com>
2024-03-28 10:50:27 +0100

    Modify Feature section

--- src/components/FeatureSection.tsx
+++ src/components/FeatureSection.tsx
@@ -3,12 +3,12 @@ import React from "react";
 
 const FeatureSection = () => {
     return (
-        <div className="container drop-shadow-2xl py-12  mx-auto xl:w-[75%]">
+        <div className="container drop-shadow-2xl px-4 md:px-0 py-12  mx-auto xl:w-[75%]">
             <div className="space-y-12">
-                <h1 className="text-center font-bold text-4xl">
+                <h1 className="text-center font-bold text-2xl md:text-4xl">
                     Featured In the Media
                 </h1>
-                <div className="flex  flex-wrap space-x-5 justify-between">
+                <div className="flex  overflow-hidden flex-wrap space-x-5 lg:justify-between justify-center">
                     <Image
                         className="f"
                         src={"/Yahoo-Finance-5.png"}
@@ -31,7 +31,7 @@ const FeatureSection = () => {
                         height={50}
                     />
                     <Image
-                        className="f"
+                        className=""
                         src={"/MarketWatch-1.png"}
                         alt=""
                         width={120}
@@ -47,17 +47,17 @@ const FeatureSection = () => {
                 </div>
             </div>
 
-            <div className="animategradient bg-no-repeat  my-32 flex relative overflow-hidden  items-center  justify-between rounded-3xl  p-12  py-24">
+            <div className="animategradient bg-no-repeat  my-32 flex relative overflow-hidden  items-center  justify-between rounded-3xl  md:p-12 p-5 md:py-24">
                 <div className="bg-opacity-15  inset-0 bg-no-repeat -top-40  absolute bg-gradientBg" />
                 <div className="relative space-y-3">
-                    <h1 className="text-5xl font-semibold leading-[1.4] text-white">
+                    <h1 className="md:text-5xl text-2xl font-semibold leading-[1.4] text-white">
                         Ready to take your hiring to the <br /> next level?
                     </h1>
-                    <p className="text-lg text-white font-semibold">
+                    <p className="md:text-lg text-white font-semibold">
                         Sign up for Loubby now! and enjoy hiring at it’s best
                     </p>
                 </div>
-                <div className="bg-white p-4 px-6 rounded-xl text-black text-center flex items-center justify-center font-medium">
+                <div className="bg-white md:p-4 p-2 md:px-6 rounded-xl text-black text-center flex items-center justify-center font-medium">
                     Get Started
                 </div>
             </div>
