6e6fd68daf362616c95736eda59ad182e8e05048
Badejo Emmanuel <badejoemmanuel805@gmail.com>
2024-04-07 20:49:32 +0100

    Create pricing page

---
+++ src/app/pricing/page.tsx
@@ -0,0 +1,14 @@
+import { Metadata } from "next";
+import React from "react";
+
+
+export const metadata: Metadata = {
+      title: "Pricing - Loubby"
+}
+
+
+const Pricing = () => {
+    return <div>Pricing</div>;
+};
+
+export default Pricing;
